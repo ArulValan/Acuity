c004c17acfde3659b7d58e61c31d9f70cb2bd23c
ArulValan <arulvalanc@gmail.com>
2019-06-10 23:54:28 +0530

    Sucessfully made that Bar chart

--- source.js
+++ source.js
@@ -3,21 +3,39 @@ Acuity = function () {
     var targetStyle = target.style;
     targetStyle.borderColor = "blue";
     targetStyle.borderStyle = "solid";
-    this.createRectangle = function (target,rectposition) {
-        var canvas=document.getElementById('myTarget').appendChild(document.createElement("canvas"));
-        var ctx=canvas.getContext("2d");
-        ctx.moveTo(rectposition.p1.x,rectposition.p1.y);
-        ctx.lineTo(rectposition.p2.x,rectposition.p2.y);
-        ctx.lineTo(rectposition.p3.x,rectposition.p3.y);
-        ctx.lineTo(rectposition.p4.x,rectposition.p4.y);
-        ctx.lineTo(rectposition.p1.x,rectposition.p1.y);
-        ctx.fill();
+    var canvas = document.getElementById('myTarget').appendChild(document.createElement("canvas"));
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    var ctx = canvas.getContext("2d");
 
+    this.createRectangle = function (target, rectposition) {
+        ctx.fillRect(...rectposition);
+    }.bind(ctx);
+
+    this.inputData = function (arg) {
+        if (arg != undefined) {
+            inputData = arg;
+        }
+        else {
+            return inputData;
+        }
     }
-    this.createRectangle(target, rectposition = {
-        p1: { x: 20, y: 20 },
-        p2: { x: 40, y: 20 },
-        p3: { x: 40, y: 25 },
-        p4: { x: 20, y: 25 }
-    })
+    //TEST BLOCK STARTS
+    this.inputData([52, 100, 43, 22, 60, 11]);
+    // swappositions
+
+
+    //TEST BLOCK ENDS
+    var no_of_bars = inputData.length;
+    var no_of_spaces = no_of_bars + 1;
+    var barXHeight = Math.floor((target.clientHeight / no_of_bars) * 0.9);
+    var barXSpace = Math.floor((target.clientHeight - no_of_bars * barXHeight) / no_of_spaces);
+    var XOffset = 10, YOffset = 0;
+    for (var i = 0; i < no_of_bars; i++) {
+        YOffset += barXSpace;
+        this.createRectangle(target, [XOffset, YOffset, inputData[i], barXHeight]);
+        YOffset += barXHeight;
+    }
 }
